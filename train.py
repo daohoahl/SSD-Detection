658e6ca1f794763e5998624f06c5d3b726977f52
HungDao <duyhung81002@gmail.com>
2025-01-14 23:10:30 +0700

    fix

--- train.py
+++ train.py
@@ -12,7 +12,6 @@ data_folder = './'  # folder with data files
 keep_difficult = True  # use objects considered difficult to detect?
 
 # Model parameters
-# Not too many here since the SSD300 has a very specific structure
 n_classes = len(label_map)  # number of different types of objects
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -20,19 +19,17 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 checkpoint = None  # path to model checkpoint, None if none
 batch_size = 8  # batch size
 iterations = 120000  # number of iterations to train
-workers = 2  # number of workers for loading data in the DataLoader
+workers = 4  # number of workers for loading data in the DataLoader
 print_freq = 200  # print training status every __ batches
 lr = 1e-3  # learning rate
 decay_lr_at = [80000, 100000]  # decay learning rate after these many iterations
 decay_lr_to = 0.1  # decay learning rate to this fraction of the existing learning rate
 momentum = 0.9  # momentum
 weight_decay = 5e-4  # weight decay
-grad_clip = None  # clip if gradients are exploding, which may happen at larger batch sizes (sometimes at 32) - you will recognize it by a sorting error in the MuliBox loss calculation
+grad_clip = None  # clip if gradients are exploding
 
 cudnn.benchmark = True
 
-# Initialize ClearML task
-task = Task.init(project_name='Object Detection with SSD', task_name='Train SSD300')
 
 def main():
     """
@@ -40,11 +37,13 @@ def main():
     """
     global start_epoch, label_map, epoch, checkpoint, decay_lr_at
 
+    # Initialize ClearML task
+    task = Task.init(project_name='SSD300 Training', task_name='Train SSD300', task_type=Task.TaskTypes.optimizer)
+    
     # Initialize model or load checkpoint
     if checkpoint is None:
         start_epoch = 0
         model = SSD300(n_classes=n_classes)
-        # Initialize the optimizer, with twice the default learning rate for biases, as in the original Caffe repo
         biases = list()
         not_biases = list()
         for param_name, param in model.named_parameters():
@@ -55,51 +54,37 @@ def main():
                     not_biases.append(param)
         optimizer = torch.optim.SGD(params=[{'params': biases, 'lr': 2 * lr}, {'params': not_biases}],
                                     lr=lr, momentum=momentum, weight_decay=weight_decay)
-
     else:
         checkpoint = torch.load(checkpoint)
         start_epoch = checkpoint['epoch'] + 1
-        print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
         model = checkpoint['model']
         optimizer = checkpoint['optimizer']
 
-    # Move to default device
     model = model.to(device)
     criterion = MultiBoxLoss(priors_cxcy=model.priors_cxcy).to(device)
 
-    # Custom dataloaders
-    train_dataset = PascalVOCDataset(data_folder,
-                                     split='train',
-                                     keep_difficult=keep_difficult)
+    train_dataset = PascalVOCDataset(data_folder, split='train', keep_difficult=keep_difficult)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
                                                collate_fn=train_dataset.collate_fn, num_workers=workers,
-                                               pin_memory=True)  # note that we're passing the collate function here
+                                               pin_memory=True)
 
-    # Calculate total number of epochs to train and the epochs to decay learning rate at (i.e. convert iterations to epochs)
-    # To convert iterations to epochs, divide iterations by the number of iterations per epoch
-    # The paper trains for 120,000 iterations with a batch size of 32, decays after 80,000 and 100,000 iterations
-    epochs = 10
+    epochs = 5
     decay_lr_at = [it // (len(train_dataset) // 32) for it in decay_lr_at]
 
-    # Epochs
     for epoch in range(start_epoch, epochs):
-
-        # Decay learning rate at particular epochs
         if epoch in decay_lr_at:
             adjust_learning_rate(optimizer, decay_lr_to)
 
-        # One epoch's training
-        train(train_loader=train_loader,
-              model=model,
-              criterion=criterion,
-              optimizer=optimizer,
-              epoch=epoch)
+        train(train_loader=train_loader, model=model, criterion=criterion, optimizer=optimizer, epoch=epoch, task=task)
 
-        # Save checkpoint
         save_checkpoint(epoch, model, optimizer)
 
+    # Final checkpoint saving in ClearML
+    task.upload_artifact(name='final_model', artifact_object=model)
+    task.upload_artifact(name='final_optimizer', artifact_object=optimizer)
 
-def train(train_loader, model, criterion, optimizer, epoch):
+
+def train(train_loader, model, criterion, optimizer, epoch, task):
     """
     One epoch's training.
 
@@ -108,39 +93,31 @@ def train(train_loader, model, criterion, optimizer, epoch):
     :param criterion: MultiBox loss
     :param optimizer: optimizer
     :param epoch: epoch number
+    :param task: ClearML task object
     """
-    model.train()  # training mode enables dropout
-
-    batch_time = AverageMeter()  # forward prop. + back prop. time
-    data_time = AverageMeter()  # data loading time
-    losses = AverageMeter()  # loss
+    model.train()  
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
 
     start = time.time()
 
-    # Batches
     for i, (images, boxes, labels, _) in enumerate(train_loader):
         data_time.update(time.time() - start)
-
-        # Move to default device
-        images = images.to(device)  # (batch_size (N), 3, 300, 300)
+        images = images.to(device)
         boxes = [b.to(device) for b in boxes]
         labels = [l.to(device) for l in labels]
 
-        # Forward prop.
-        predicted_locs, predicted_scores = model(images)  # (N, 8732, 4), (N, 8732, n_classes)
+        predicted_locs, predicted_scores = model(images)
 
-        # Loss
-        loss = criterion(predicted_locs, predicted_scores, boxes, labels)  # scalar
+        loss = criterion(predicted_locs, predicted_scores, boxes, labels)
 
-        # Backward prop.
         optimizer.zero_grad()
         loss.backward()
 
-        # Clip gradients, if necessary
         if grad_clip is not None:
             clip_gradient(optimizer, grad_clip)
 
-        # Update model
         optimizer.step()
 
         losses.update(loss.item(), images.size(0))
@@ -148,16 +125,18 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         start = time.time()
 
-        # Print status
         if i % print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i, len(train_loader),
-                                                                  batch_time=batch_time,
-                                                                  data_time=data_time, loss=losses))
-    del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
+            print(f'Epoch: [{epoch}][{i}/{len(train_loader)}]\t'
+                  f'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  f'Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  f'Loss {losses.val:.4f} ({losses.avg:.4f})\t')
 
+            # Log metrics to ClearML
+            task.log_metric('loss', losses.avg, step=epoch * len(train_loader) + i)
+            task.log_metric('batch_time', batch_time.avg, step=epoch * len(train_loader) + i)
+            task.log_metric('data_time', data_time.avg, step=epoch * len(train_loader) + i)
+
+    del predicted_locs, predicted_scores, images, boxes, labels
 
 if __name__ == '__main__':
     main()
